30761092297e504c36474f4f829c312bd37ce6e0
SlavaLooking <lukinsyatoslav@gmail.com>
2026-03-05 09:55:08 -0500

    backend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, '0.0.0.0');
+  app.enableCors() // для работы CORS, чтобы браузеры и мобильные приложения не блокировали запросы к вашему API.
+  await app.listen(3000, '0.0.0.0'); // Слушать все адреса
 }
 bootstrap();
